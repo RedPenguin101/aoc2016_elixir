84f13cf2f4b577f47de3e94ae96782ef7c4e7d48
Joe Lezala <allaboutthatmace1789@gmail.com>
2022-11-18 12:56:07 +0000

    day5 tweak

--- lib/day05.ex
+++ lib/day05.ex
@@ -28,8 +28,8 @@ defmodule Aoc2016.Day05 do
   def find_password_part1(input, found, num) do
     case good_hash?(:erlang.md5(input <> Integer.to_string(num))) do
       {:no}        -> find_password_part1(input, found, num+1)
-      {:ok1, c}    -> find_password_part1(input, [c | found], num+1)
-      {:ok2, c, _} -> find_password_part1(input, [c | found], num+1)
+      {:ok, c}    -> find_password_part1(input, [c | found], num+1)
+      {:ok, c, _} -> find_password_part1(input, [c | found], num+1)
     end
   end
 
@@ -37,9 +37,9 @@ defmodule Aoc2016.Day05 do
   def find_password_part2(_, found, _) when map_size(found) == 8, do: found
   def find_password_part2(input, found, num) do
     case good_hash?(:erlang.md5(input <> Integer.to_string(num))) do
-      {:no}        -> find_password_part2(input, found, num+1)
-      {:ok1, _}    -> find_password_part2(input, found, num+1)
-      {:ok2, c, d} -> find_password_part2(input, Map.put_new(found, c, d), num+1)
+      {:no}       -> find_password_part2(input, found, num+1)
+      {:ok, _}    -> find_password_part2(input, found, num+1)
+      {:ok, c, d} -> find_password_part2(input, Map.put_new(found, c, d), num+1)
     end
   end
 
@@ -47,8 +47,8 @@ defmodule Aoc2016.Day05 do
     <<a,b,c,d,_::binary>> = binary
     cond do
       a>0 || b>0 -> {:no}
-      c < 8      -> {:ok2, c, Bitwise.>>>(d,4)}
-      c <= 0xF   -> {:ok1, c}
+      c < 8      -> {:ok, c, Bitwise.>>>(d,4)}
+      c <= 0xF   -> {:ok, c}
       c > 0xF    -> {:no}
     end
   end
