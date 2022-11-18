37da84a06f6d273c33fbf71d2f742e08d43ddaa3
Joe Lezala <allaboutthatmace1789@gmail.com>
2022-11-18 15:09:18 +0000

    day08 tidy

--- lib/day08.ex
+++ lib/day08.ex
@@ -1,12 +1,8 @@
 defmodule Day08 do
   import Enum
 
-  @example ["rect 3x2","rotate column x=1 by 1", "rotate row y=0 by 4", "rotate column x=1 by 1"]
-  @re ~r/(\d+)[a-z| ]+(\d+)/
-  #@rows 3 #example
-  @rows 6
-  #@cols 7 #example
   @cols 50
+  @rows 6
 
   def update_tuple(tuple, idx, fun) do
     current = elem(tuple, idx)
@@ -14,7 +10,6 @@ defmodule Day08 do
   end
 
   def part1 do
-    @example
     File.read!("data/day08.txt")
     |> String.trim
     |> String.split("\n")
@@ -24,7 +19,6 @@ defmodule Day08 do
   end
 
   def part2 do
-    @example
     File.read!("data/day08.txt")
     |> String.trim
     |> String.split("\n")
@@ -33,24 +27,24 @@ defmodule Day08 do
     |> draw
   end
 
-  def proc_instr({:rect, {cols,rows}}, screen), do: rect(screen, cols, rows)
+  def proc_instr({:rect,      {cols,rows}}, screen),  do: rect(screen, cols, rows)
   def proc_instr({row_or_col, {num,amount}}, screen), do: rotate(screen, row_or_col, num, amount)
 
   def rect(screen, cols, rows) do
     coords = for x <- 0..cols-1, y <-0..rows-1 do
-      {x, y}
-    end
+               {x, y}
+             end
 
     reduce(coords, screen, fn coord, scrn -> Map.put(scrn, coord, 1) end)
   end
 
   def rotate(screen, row_or_col, num, amount) do
     {f, m, wrap} = case row_or_col do
-                           :col -> {0, 1, @rows}
-                           :row -> {1, 0, @cols}
-                         end
+                     :col -> {0, 1, @rows}
+                     :row -> {1, 0, @cols}
+                   end
 
-    push = fn a -> rem(a+amount, wrap) end
+    push = fn curr -> rem(curr + amount, wrap) end
 
     change = Map.filter(screen, fn {c, _} -> elem(c,f) == num end)
     change
@@ -71,17 +65,15 @@ defmodule Day08 do
   end
 
   def parse_row(string) do
-    case string do
-      "rect " <> rest -> {:rect, get_integers(rest)}
-      "rotate column x=" <> rest -> {:col, get_integers(rest)}
-      "rotate row y=" <> rest -> {:row, get_integers(rest)}
-    end
-  end
+    nums = Regex.run(~r/(\d+)[a-z| ]+(\d+)/, string)
+           |> tl
+           |> map(&String.to_integer/1)
+           |> List.to_tuple
 
-  def get_integers(string) do
-    Regex.run(@re, string)
-    |> tl
-    |> map(&String.to_integer/1)
-    |> List.to_tuple
+    case string do
+      "rect "    <> _ -> {:rect, nums}
+      "rotate c" <> _ -> {:col, nums}
+      "rotate r" <> _ -> {:row, nums}
+    end
   end
 end
